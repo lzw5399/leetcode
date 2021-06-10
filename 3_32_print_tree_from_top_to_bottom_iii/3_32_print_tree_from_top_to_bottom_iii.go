7be056e67d52f255568c3cd37ba4a1fad3600c91
zhiwen <lzw5399@gmail.com>
2021-06-10 11:42:07 +0800

    feat: print tree from top to bottom

--- 3_32_print_tree_from_top_to_bottom_iii/3_32_print_tree_from_top_to_bottom_iii.go
+++ 3_32_print_tree_from_top_to_bottom_iii/3_32_print_tree_from_top_to_bottom_iii.go
@@ -1,12 +1,12 @@
 /**
  * @Author: lzw5399
  * @Date: 2021/6/10 0:38
- * @Desc: 剑指 Offer 32 - II. 从上到下打印二叉树 II
- * @Desc: https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-ii-lcof/
- * @Desc: 从上到下按层打印二叉树，同一层的节点按从左到右的顺序打印，每一层打印到一行。
+ * @Desc: 剑指 Offer 32 - III. 从上到下打印二叉树 III
+ * @Desc: https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-iii-lcof/
+ * @Desc: 请实现一个函数按照之字形顺序打印二叉树，即第一行按照从左到右的顺序打印，第二层按照从右到左的顺序打印，第三行再按照从左到右的顺序打印，其他行以此类推。
  * @Desc: 例如: 给定二叉树: [3,9,20,null,null,15,7]
- * @Desc: 返回 [[3], [9,20], [15,7]]
- * @Desc: [思路]: 还是借助一个队列进行层序遍历，对每一层
+ * @Desc: 返回 [[3], [20, 9], [15,7]]
+ * @Desc: [思路]: 还是借助一个队列进行层序遍历
  */
 package main
 
@@ -25,17 +25,24 @@ func levelOrder(root *TreeNode) [][]int {
 	}
 
 	queue := []*TreeNode{root}
+	level := 0
 	for len(queue) != 0 {
 		var temp []int
 
 		length := len(queue)
+
+		level++
 		for i := 0; i < length; i++ {
 			// 获取node
 			node := queue[0]
 			queue = queue[1:]
 
 			// 偶数层prepend ！！！
-			temp = append(temp, node.Val)
+			if level%2 == 0 {
+				temp = append([]int{node.Val}, temp...)
+			} else { // 奇数层append
+				temp = append(temp, node.Val)
+			}
 
 			if node.Left != nil {
 				queue = append(queue, node.Left)
